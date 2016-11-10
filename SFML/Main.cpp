46323de6d08ecd7e1fc676e5737af30624c298b1
paddy <paddyw07_93@hotmail.com>
2016-11-09 22:00:20 -0400

    file changes/adds/removes

--- SFML/Main.cpp
+++ SFML/Main.cpp
@@ -12,21 +12,12 @@
 
 int main()
 {
-	//teststtiashgfkajsdhfkajsghdfkjadg
-
-
-	/*float a = 180;
-	for (int i = 0; i<20; i++)
-		printf("%f\n", ((float)rand() / (float)(RAND_MAX)) * a + 180);*/
-
 	char path[MAX_PATH];
 	GetCurrentDirectoryA(MAX_PATH, path);
 	std::cout << "Current Directory = " << path << std::endl;
 
 	Game game;
-
 	game.gameState.push(new GameStateLevel(&game));
-	
 	game.run();
 
 	return 0;
